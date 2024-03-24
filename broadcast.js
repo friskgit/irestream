63977fd560ac9260735ac55f87bc63aa45221c4b
Rikard Vilhelm Lindell <rikard@c3n.se>
2024-03-24 12:33:54 +0100

    made a minor change to push

--- broadcast.js
+++ broadcast.js
@@ -1,6 +1,8 @@
 const wrtc = require('wrtc');
 const io = require('socket.io')(server);
 
+// made a minor change to push.
+
 io.on('connection', function(socket) {
   const pc = new wrtc.RTCPeerConnection();
 
